b39c7cd69229d4d49b9c84d1ad16726b7d6f458f
Aditeya Baral <aditeya.baral@gmail.com>
2021-03-22 18:23:34 +0530

    fix deploy bug

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app.db:app
+web: gunicorn app.database:app
--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from .utils import *
 import dotenv
 import datetime
-from flask import Flask
+from flask import Flask, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 
@@ -14,6 +14,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+
 class Repository(db.Model):
     __tablename__ = "repository"
     _id = db.Column(db.String(26), primary_key=True)
@@ -21,7 +22,8 @@ class Repository(db.Model):
     url = db.Column(db.String(1024), nullable=False)
     create_time = db.Column(db.DateTime, nullable=False)
     visibility = db.Column(db.String(10), nullable=False)
-    owner_id = db.Column(db.String(1024), ForeignKey('user._id', ondelete='CASCADE'), nullable=False)
+    owner_id = db.Column(db.String(1024), ForeignKey(
+        'user._id', ondelete='CASCADE'), nullable=False)
     # user_relation = relationship("user", back_populates="repository")
 
     def __init__(self, repo_name, visibility) -> None:
@@ -49,8 +51,10 @@ class User(db.Model):
 class OwnerRepositoryRelation(db.Model):
     __tablename__ = "repositoryuserelation"
     _id = db.Column(db.String(26), primary_key=True, autoincrement=True)
-    user_id = db.Column(db.String(26), ForeignKey('user._id'), nullable=False)  # Check cascade
-    repository_id = db.Column(db.String(26), ForeignKey('repository._id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.String(26), ForeignKey(
+        'user._id'), nullable=False)  # Check cascade
+    repository_id = db.Column(db.String(26), ForeignKey(
+        'repository._id', ondelete='CASCADE'), nullable=False)
     relation = db.Column(db.String(20), nullable=False)
     # user_relation = relationship("user", back_populates="repositoryuserelation")
     # repository_relation = relationship("repository", back_populates="repositoryuserelation")
@@ -59,6 +63,11 @@ class OwnerRepositoryRelation(db.Model):
         self.user_id = user_id
         self.repository_id = repository_id
         self.relation = relation
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return redirect("https://github.com/aditeyabaral/version-control", 302)
 
 
 if __name__ == "__main__":
