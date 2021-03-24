69a92a44a114650bef1f7ca755b5b9188a437553
Aditeya Baral <aditeya.baral@gmail.com>
2021-03-24 19:39:01 +0530

    push updated db

--- app/database.py
+++ app/database.py
@@ -11,17 +11,17 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    __tablename__ = "user"
+class Developer(db.Model):
+    __tablename__ = "developer"
     _id = db.Column(db.String(26), primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
-    def __init__(self, name, email, password, user_id) -> None:
+    def __init__(self, name, email, password, developer_id) -> None:
         self.username = name
         self.email = email
-        self._id = user_id  # Check database
+        self._id = developer_id
         self.password = password
 
 
@@ -29,57 +29,92 @@ class Repository(db.Model):
     __tablename__ = "repository"
     _id = db.Column(db.String(26), primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(100))
+    description = db.Column(db.String(150))
     url = db.Column(db.String(1024), nullable=False)
     create_time = db.Column(db.DateTime, nullable=False)
     visibility = db.Column(db.String(10), nullable=False)
     owner_id = db.Column(db.String(1024), db.ForeignKey(
-        'user._id', ondelete='CASCADE'), nullable=False)
+        'developer._id', ondelete='CASCADE'), nullable=False)
     # user_relation = db.relationship("user", back_populates="repository")
 
     def __init__(self, repo_name, visibility, repo_id, url, owner_id, create_time, description=None) -> None:
+        self._id = repo_id
         self.name = repo_name
-        self.visibility = visibility
-        self._id = repo_id  # check with database
+        self.description = description
         self.url = url
         self.create_time = create_time
-        self.description = description
+        self.visibility = visibility
         self.owner_id = owner_id
 
 
 class OwnerRepositoryRelation(db.Model):
     __tablename__ = "repositoryuserelation"
-    user_id = db.Column(db.String(26), db.ForeignKey(
-        'user._id'), primary_key=True)  # Check cascade
+    developer_id = db.Column(db.String(26), db.ForeignKey(
+        'developer._id'), primary_key=True)  # Check cascade
     repository_id = db.Column(db.String(26), db.ForeignKey(
         'repository._id', ondelete='CASCADE'), primary_key=True)
     relation = db.Column(db.String(20), nullable=False)
     # user_relation = db.relationship("user", back_populates="repositoryuserelation")
     # repository_relation = db.relationship("repository", back_populates="repositoryuserelation")
 
-    def __init__(self, user_id, repository_id, relation) -> None:
-        self.user_id = user_id
+    def __init__(self, developer_id, repository_id, relation) -> None:
+        self.developer_id = developer_id
         self.repository_id = repository_id
         self.relation = relation
 
 
 class File(db.Model):
     __tablename__ = "file"
+    _id = db.Column(db.String(26), primary_key=True)
     path = db.Column(db.String(26), nullible=False, primary_key=True)
-    repository_id = db.Column(db.String(26), nullable=False, primary_key=True)
+    repository_id = db.Column(db.String(26), db.ForeignKey(
+        'repository._id', ondelete='CASCADE'), nullable=False, primary_key=True)
     status = db.Column(db.String(15), nullable=False)
-    last_modified = db.Column(db.DateTime, nullable=False)
+    last_modified = db.Column(db.DateTime)
     last_committed = db.Column(db.DateTime)
     last_pushed = db.Column(db.DateTime)
-    
 
-    def __init__(self, path, repository_id, status="unchanged", last_modified=None, last_committed=None, last_pushed=None) -> None:
+    def __init__(self, _id, path, repository_id, status, last_modified=None, last_committed=None, last_pushed=None) -> None:
         self.path = path
         self.repository_id = repository_id
         self.status = status
         self.last_modified = last_modified
         self.last_committed = last_committed
         self.last_pushed = last_pushed
+
+
+class Commit(db.Model):
+    __tablename__ = "commit"
+    _id = db.Column(db.String(26), primary_key=True)
+    developer_id = db.Column(db.String(26), db.ForeignKey(
+        'developer._id', ondelete='CASCADE'))
+    repository_id = db.Column(db.String(26), db.ForeignKey(
+        'repository._id', ondelete='CASCADE'), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    message = db.Column(db.String(100))
+    file_id = db.Column(db.String(26), db.ForeignKey(
+        'file._id', ondelete='CASCADE'), primary_key=True)
+
+    def __init__(self, _id, developer_id, repository_id, time, file_id, message=None) -> None:
+        self._id = _id
+        self.developer_id = developer_id
+        self.repository_id = repository_id
+        self.time = time
+        self.file_id = file_id
+        self.message = message
+
+
+class Push(db.Model):
+    __tablename__ = "push"
+    _id = db.Column(db.String(26), primary_key=True)
+    commit_id = db.Column(db.String(26), db.ForeignKey(
+        'commit._id', ondelete='CASCADE'), primary_key=True)
+    time = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, _id, commit_id, time) -> None:
+        self._id = _id
+        self.commit_id = commit_id
+        self.time = time
 
 
 @app.route("/", methods=["GET", "POST"])
--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,10 @@ import string
 import random
 import os
 
+
 def getRandomID():
-    random_id = "".join(ch for ch in random.choice(string.ascii_letters+string.digits))
+    random_id = "".join(ch for ch in random.choice(
+        string.ascii_letters+string.digits))
     return random_id
 
 
@@ -12,17 +14,8 @@ def getRepoURL(repo_name, repo_id):
 
 
 def nano(filename):
-    '''
-    lines = list()
-    while True:
-        try:
-            line = input()
-            lines.append(line)
-        except EOFError:
-            break
-    return '\n'.join(lines)
-    '''
     os.system(f"micro {filename}")
+
 
 def cat(filename):
     os.system(f"cat {filename}")
@@ -30,6 +23,7 @@ def cat(filename):
 
 def ls(path="."):
     print("\n".join(os.listdir(path)))
+
 
 def mkdir(dirname):
     cur_path = os.getcwd()
@@ -39,17 +33,9 @@ def mkdir(dirname):
     except:
         print("Error while creating directory!")
 
+
 def cd(path):
     try:
         os.chdir(path)
     except (WindowsError, OSError):
         print("Error while changing path")
-
-#nano("hello.py")
-#cat("hello.py")
-
-#ls("../")
-    
-
-#content = nano()
-#print(content)
