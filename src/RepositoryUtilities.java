33210c5d34745b45dffe62df2892331ebc5204c5
Aditeya <aditeya.baral@gmail.com>
2022-04-16 12:22:44 +0530

    Added some repoUtils stuff

--- src/RepositoryUtilities.java
+++ src/RepositoryUtilities.java
@@ -1,13 +1,14 @@
 import java.io.*;
 import java.sql.*;
 import java.util.*;
-// import java.lang.*;
+import java.lang.*;
+import com.fasterxml.uuid.*;
 
 class RepositoryUtilities
 {
     public String getRepositoryIdFromLocalClone()
     {
-        File file = new File(".coffee/bean");
+        File file = new File(".coffee/.bean");
         if (file.exists())
         {
             try
@@ -31,14 +32,17 @@ class RepositoryUtilities
     {
         try
         {
-            Process p = Runtime.getRuntime().exec("diff " + file1 + " " + file2);
+            Process p = Runtime.getRuntime().exec("diff -u " + file1 + " " + file2);
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String line = br.readLine();
             StringBuilder sb = new StringBuilder();
             while (line != null)
             {
-                sb.append(line);
-                sb.append("\n");
+                if ((line.startsWith("+") || line.startsWith("-")) && !(line.startsWith("+++") || line.startsWith("---")))
+                {
+                    sb.append(line);
+                    sb.append("\n");
+                }
                 line = br.readLine();
             }
             return sb.toString();
@@ -49,13 +53,93 @@ class RepositoryUtilities
             return null;
         }
     }
-}
 
-class TestRepositoryUtilities
-{
-    public static void main(String[] args) throws Exception
+    public HashMap checkFileIsModified(String diff)
     {
-        RepositoryUtilities ru = new RepositoryUtilities();
-        System.out.println(ru.getRepositoryIdFromLocalClone());
+        HashMap<String, Integer> diffMap = new HashMap<String, Integer>();
+        if (diff.length() < 0)
+        {
+            diffMap.put("modified", 0);
+            return diffMap;
+        }
+        else
+        {
+            String[] lines = diff.split("\n");
+            int additions = 0;
+            int deletions = 0;
+            for (String line : lines)
+            {
+                if (line.startsWith("+") || line.startsWith("-") || line.startsWith("?"))
+                {
+                    if (line.startsWith("+")) additions++;
+                    else if (line.startsWith("-")) deletions++;
+                }
+            }
+            if (additions > 0 || deletions > 0)
+            {
+                diffMap.put("modified", 1);
+                diffMap.put("additions", additions);
+                diffMap.put("deletions", deletions);
+            }
+            else diffMap.put("modified", 0);
+            return diffMap;
+        }
+    }
+
+    public String generateRepositoryID()
+    {
+        return Generators.timeBasedGenerator().generate().toString();
+    }
+
+    public String generateFileID()
+    {
+        return Generators.timeBasedGenerator().generate().toString();
+    }
+
+    public String generateCommitID()
+    {
+        return Generators.timeBasedGenerator().generate().toString();
+    }
+
+    public void createBeanFile(String userId, String repoName, String repoId, String description, String url, LocalDateTime createTime, String visibility)
+    {
+        try
+        {
+            File file = new File(".coffee/.bean");
+            if (!file.exists()) file.createNewFile();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write("repositoryId," + repoId); bw.newLine();
+            bw.write("repositoryName," + repoName); bw.newLine();
+            bw.write("userId," + userId); bw.newLine();
+            bw.write("url," + url); bw.newLine();
+            bw.write("description," + description); bw.newLine();
+            bw.write("createTime," + createTime.toString()); bw.newLine();
+            bw.write("visibility," + visibility); bw.newLine();
+            bw.write("collaborators,"); bw.newLine();
+            bw.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public String getRepositoryOwner(Coffee coffee, String repositoryId)
+    {
+        return null; // implement RelationDatabaseUtilities first
     }
 }
+
+// class TestRepositoryUtilities
+// {
+//     public static void main(String[] args) throws Exception
+//     {
+//         RepositoryUtilities ru = new RepositoryUtilities();
+//         System.out.println(ru.getRepositoryIdFromLocalClone());
+//         String diff = ru.getDiffBetweenFiles("repoUtils.py", "repoUtils2.py");
+//         HashMap<String, Integer> diffMap = ru.checkFileIsModified(diff);
+//         System.out.println(diffMap);
+//         String repoId = ru.generateRepositoryID();
+//         System.out.println(repoId);
+//     }
+// }
--- src/repoUtils.py
+++ src/repoUtils.py
@@ -15,87 +15,6 @@ relationDB = relationDBUtils()
 userDB = userDBUtils()
 
 
-
-# def getRepoIdFromDirectory():
-#     with open(f".togepi/tgpinfo.txt") as f:
-#         content = f.read().strip().split('\n')
-#         _, repo_id = content[0].split(',')
-#         repo_id = repo_id.strip()
-#     return repo_id
-
-
-def getDiff(cloud_file_content, local_file_content):
-    local_file_content_lines = local_file_content.split('\n')
-    cloud_file_content_lines = cloud_file_content.split('\n')
-    d = difflib.Differ()
-    diff = d.compare(cloud_file_content_lines, local_file_content_lines)
-    diff = '\n'.join(diff)
-    return diff
-
-
-def checkFileIsModified(diff):
-    lines = diff.split('\n')
-    adds = 0
-    dels = 0
-    for l in lines:
-        if l.startswith('+ ') or l.startswith('- ') or l.startswith('? '):
-            if l.startswith('+ '):
-                adds += 1
-            elif l.startswith('- '):
-                dels += 1
-            else:
-                pass
-    if adds > 0 or dels > 0:
-        return True, adds, dels
-    return False, 0, 0
-
-
-def generateRepositoryID():
-    all_repos = repoDB.getAllRepositoryID()
-    if not all_repos:
-        repo_id = "REPO000001"
-    else:
-        roll_numbers = sorted([int(i[0][6:]) for i in all_repos])
-        new_roll_number = str(roll_numbers[-1] + 1)
-        repo_id = f"REPO{new_roll_number.zfill(6)}"
-    return repo_id
-
-
-def generateFileID():
-    all_files = fileDB.getAllFileID()
-    if not all_files:
-        file_id = "FILE000001"
-    else:
-        roll_numbers = sorted([int(i[0][6:]) for i in all_files])
-        new_roll_number = str(roll_numbers[-1] + 1)
-        file_id = f"FILE{new_roll_number.zfill(6)}"
-    return file_id
-
-
-def generateCommitID():
-    all_commits = commitDB.getAllCommitID()
-    if not all_commits:
-        commit_id = "COMMIT000001"
-    else:
-        roll_numbers = sorted([int(i[0][6:]) for i in all_commits])
-        new_roll_number = str(roll_numbers[-1] + 1)
-        commit_id = f"COMMIT{new_roll_number.zfill(6)}"
-    return commit_id
-
-
-def createInfoFile(user_id, repo_name, repo_id, description, url, create_time, visibility):
-    content = f'''repository_id,{repo_id}
-repository_name,{repo_name}
-user_id,{user_id}
-url,{url}
-description,{description}
-create_time,{str(create_time)},
-visibility,{visibility}
-collaborators,'''
-
-    with open(f"{repo_name}/.togepi/tgpinfo.txt", "w") as f:
-        f.write(content)
-
 def getRepoOwner(repo_id):
     relations = relationDB.getAllRelations(repo_id)
     owner = ''
@@ -146,42 +65,6 @@ def init(cache, repo_name):
     local_path = os.path.join(os.getcwd(), repo_name)
     fsUtils.uploadFolder(local_path, dropbox_path)
     return True
-
-
-def initGUI(cache, repo_name, description=None, visibility="public"):
-    user_id = cache["current_user_id"]
-    username = cache["current_username"]
-    if repoDB.checkUserRepositoryExists(user_id, repo_name):
-        print("Repository names have to be unique per user.")
-        return False, 1
-    if len(repo_name) > 50:
-        print("Repository Name cannot be over 50 chars long")
-        return False, 2
-    cliUtils.mkdir(repo_name)
-    return_path = os.path.join(os.getcwd(), repo_name)
-    cliUtils.mkdir(os.path.join(repo_name, ".togepi"))
-    repo_id = generateRepositoryID()
-    #     description = input("Enter repository description (under 150 chars): ")
-    #     if len(description) > 150:
-    #         print("Description must be under 150 chars. Truncating description..")
-    #         description = description[:150]
-    url = f"/{username}/{repo_name}"
-    create_time = datetime.utcnow()
-    # if visibility not in ["public", "private"]:
-    #     print("Invalid option. Resetting to public.")
-    #     visibility = "public"
-
-    createInfoFile(user_id, repo_name, repo_id, description,
-                   url, create_time, visibility)
-    repoDB.createRepository(user_id, repo_name, repo_id,
-                             description, url, create_time, visibility)
-    relationDB.createUserRepositoryRelation(user_id, repo_id)
-
-    dropbox_path = f"/{username}/{repo_name}/"
-    # before uploading track the info file
-    local_path = os.path.join(os.getcwd(), repo_name)
-    fsUtils.uploadFolder(local_path, dropbox_path)
-    return True, (repo_id, repo_name, user_id, username), return_path
 
 
 def add(cache, filepaths):
